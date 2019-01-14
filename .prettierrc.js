9e71843cd5a14743d3845bfbd7dac55a871b3067
Ryan Crosser <ryankcrosser@gmail.com>
2019-01-14 16:26:51 -0500

    feat: Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 200,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none', // none|es5|all
+  bracketSpacing: true,
+  arrowParens: 'avoid', //, avoid|always
+  proseWrap: 'preserve' // always|never|preserve
+};
---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+<a name="1.0.0"></a>
+# 1.0.0 (2018-12-07)
+
+
+### Features
+
+* Initial commit of conventional changelog f0bc40d
+
+
+
